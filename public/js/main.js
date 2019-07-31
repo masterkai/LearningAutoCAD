fa08b50b6bccbc8349e1dfe98b140bd7f34b9849
masterkai <kai0919@hotmail.com>
2019-07-31 20:56:57 +0800

    tag to show output complete!

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,29 @@
 $(function () {
-    console.log(`It's OK!!!`);
+  console.log(`It's OK!!!`);
 
-    $('.tagBall').cloudTag({ballSize:150});
+  let outputOdd = $('.output:odd'),
+    outputEven = $('.output:even'),
+    output = $('.output')
 
+  $('.tagBall').cloudTag({ballSize: 150});
+
+  let odd = $('.tag:odd')
+  console.log(odd);
+  let i = 0
+  odd.on('click', () => {
+    i++
+    output.removeClass('active')
+    outputOdd.addClass('active')
+    console.log(`it's odd~~!${i} times~~`);
+  })
+
+  let even = $('.tag:even')
+  console.log(even);
+  let x = 0
+  even.on('click', () => {
+    x++
+    output.removeClass('active')
+    outputEven.addClass('active')
+    console.log(`it's even~~!${x} times~~`);
+  })
 })
