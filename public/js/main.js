690d5cfe0799f633ce812ac5f54a698517b802d0
masterkai <kai0919@hotmail.com>
2019-07-31 22:34:06 +0800

    animating the prograss bar

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,27 @@ $(function () {
   console.log(odd);
   let i = 0
   odd.on('click', () => {
+    $('.output:odd .bar').animate({
+      flexBasis: '0'
+    },0)
+    $('.output:odd .bar:eq(0)').animate({
+      flexBasis: '40%'
+    },2500)
+    $('.output:odd .bar:eq(1)').animate({
+      flexBasis: '58%'
+    },2500)
+    $('.output:odd .bar:eq(2)').animate({
+      flexBasis: '50%'
+    },2500)
+    $('.output:odd .bar:eq(3)').animate({
+      flexBasis: '55%'
+    },2500)
+    $('.output:odd .bar:eq(4)').animate({
+      flexBasis: '50%'
+    },2500)
+    $('.output:odd .bar:eq(5)').animate({
+      flexBasis: '43%'
+    },2500)
     i++
     output.removeClass('active')
     outputOdd.addClass('active')
@@ -21,9 +42,33 @@ $(function () {
   console.log(even);
   let x = 0
   even.on('click', () => {
+    $('.output:even .bar').animate({
+      flexBasis: '0'
+    },0)
+    $('.output:even .bar:eq(0)').animate({
+      flexBasis: '76%'
+    },2500)
+    $('.output:even .bar:eq(1)').animate({
+      flexBasis: '43%'
+    },2500)
+    $('.output:even .bar:eq(2)').animate({
+      flexBasis: '48%'
+    },2500)
+    $('.output:even .bar:eq(3)').animate({
+      flexBasis: '55%'
+    },2500)
+    $('.output:even .bar:eq(4)').animate({
+      flexBasis: '50%'
+    },2500)
+    $('.output:even .bar:eq(5)').animate({
+      flexBasis: '42%'
+    },2500)
+
     x++
     output.removeClass('active')
     outputEven.addClass('active')
     console.log(`it's even~~!${x} times~~`);
   })
+
+
 })
